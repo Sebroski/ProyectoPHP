170c28c238039ffdd61bc0a99efe221adb6c2446
Sebastian Gonzalez <seax27@gmail.com>
2025-10-24 00:55:34 -0300

    fix db_config

--- db_config.php
+++ db_config.php
@@ -2,10 +2,9 @@
 // Configuración de la Base de Datos (PostgreSQL)
 define('DB_HOST', 'localhost');
 define('DB_PORT', '5432'); // Puerto por defecto de PostgreSQL
-define('DB_NAME', 'tu_base_de_datos');
-define('DB_USER', 'tu_usuario');
-define('DB_PASS', 'tu_contraseña');
-
+define('DB_NAME', 'productos_db'); // El nombre que le diste
+define('DB_USER', 'postgres');     // El usuario por defecto
+define('DB_PASS', 'pollo2001'); // La contraseña que pusiste al instalar PostgreSQL
 function getDBConnection() {
     $dsn = "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
     
